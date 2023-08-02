966a09435026a2f0916997dbd5871d690029416a
Damien <DamienB2474@protonmail.com>
2023-08-02 19:15:26 -0400

    Added React Bootstrap

--- src/routes/home/Home.tsx
+++ src/routes/home/Home.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import Button from 'react-bootstrap/Button';
+
 
 export default function Home() {
   return (
-    <div>Welcome to the home page!</div>
+    <div>
+      <Button variant="primary">click me</Button>
+      <br />
+      Welcome to the home page!
+    </div>
   )
 }
