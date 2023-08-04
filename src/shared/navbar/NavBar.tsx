8c26ab52d0f054ffbe32ce399bfb1ce72bfa2063
Neil <elkadineil@gmail.com>
2023-08-03 20:47:22 -0400

    refactor: trying not to use bootstrap

--- src/shared/navbar/NavBar.tsx
+++ src/shared/navbar/NavBar.tsx
@@ -1,23 +1,20 @@
 import React from 'react'
 import "./NavBar.scss"
-import { Container, Nav, Navbar } from 'react-bootstrap'
 
 export default function NavBar() {
   return (
-    <Navbar className="nav-bar" sticky='top'>
-    <Container>
-      <Navbar.Brand href="/" className='nav-brand'>
-        <img alt="" src="./logo512.png" width="30" height="30" className="d-inline-block align-top"/>{' '}
-        VTechClub
-      </Navbar.Brand>
-      <Nav className='me-auto px-4'>
-        <Nav.Link href='/' className='nav-link'>Home</Nav.Link>
-        <Nav.Link href='/events'>Events</Nav.Link>
-        <Nav.Link href='/about-us'>About Us</Nav.Link>
-        <Nav.Link href='/contact'>Contact</Nav.Link>
-        <Nav.Link href='/memebrs'>Members</Nav.Link>
-      </Nav>
-    </Container>
-  </Navbar>
+    <div className='nav-bar'>
+      <div className='nav-brand'>
+        <img src='./logo512.png' alt='VTechLogo'/>
+        VTech Club
+      </div>
+      <nav>
+        <a href='/'>Home</a>
+        <a href='/events'>Events</a>
+        <a href='/members'>Members</a>
+        <a href='/about-us'>About Us</a>
+        <a href='/contact'>Contact</a>
+      </nav>
+    </div>
   )
 }
