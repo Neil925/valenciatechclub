7fac0c4f7edd873d691342785608e85d098f47be
Damien <DamienB2474@protonmail.com>
2023-08-08 00:16:34 -0400

    updated cards to use config

--- src/routes/members/Members.tsx
+++ src/routes/members/Members.tsx
@@ -1,13 +1,13 @@
 import React from "react";
 import MemberCard from "../../components/memberCard/MemberCard";
-import membersData from "./test.json";
+import config from '../../config.json';
 import "./Members.scss";
 
 export default function Members() {
   return (
     <div className="members">
       <div className="member-cards">
-        {membersData.map((member, index) => (
+        {config.MembersList.map((member, index) => (
           <MemberCard key={index} name={member.name} role={member.role} description={member.description} />
         ))}
       </div>
