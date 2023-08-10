a68a17490dc7642af0734906c3d38d9d861ece2f
Damien <DamienB2474@protonmail.com>
2023-08-09 22:43:27 -0400

    Members page UI update & config image update

--- src/components/memberCard/MemberCard.tsx
+++ src/components/memberCard/MemberCard.tsx
@@ -5,12 +5,14 @@ type MemberProp = {
   name: string;
   role: string;
   description: string;
+  image: string
 };
 
 const MemberCard = (props: MemberProp) => {
   return (
     <div className="card">
-      <img className="profile-picture" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.cb08UhKHU2XF7BRKhq8jawHaHZ%26pid%3DApi&f=1&ipt=61ca1f1e1228619ff5f967454c632c25a3db4e893b9e02b9c7c62f789e044a70&ipo=images" />
+      {/* <img className="profile-picture" src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.cb08UhKHU2XF7BRKhq8jawHaHZ%26pid%3DApi&f=1&ipt=61ca1f1e1228619ff5f967454c632c25a3db4e893b9e02b9c7c62f789e044a70&ipo=images" /> */}
+      <img className="profile-picture" src={props.image} />
       <div className="card-content">
         <h2 className="card-title">{props.name}</h2>
         <h3 className="card-subtitle">{props.role}</h3>
--- src/routes/members/Members.tsx
+++ src/routes/members/Members.tsx
@@ -1,14 +1,56 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import MemberCard from "../../components/memberCard/MemberCard";
-import config from '../../config.json';
+import config from "../../config.json";
 import "./Members.scss";
 
+interface Member {
+  name: string;
+  role: string;
+  description: string;
+  image: string;
+}
+
+interface Config {
+  MembersList: Member[];
+}
+
 export default function Members() {
+  const { MembersList } = config as Config;
+  const [loadedImages, setLoadedImages] = useState<{ [key: string]: string }>({});
+
+  useEffect(() => {
+    Promise.all(
+      MembersList.map((member) =>
+        import(`../../img/${member.image}`).then((image) => {
+          setLoadedImages((prevImages) => ({
+            ...prevImages,
+            [member.name]: image.default,
+          }));
+        })
+      )
+    );
+  }, [MembersList]);
+
   return (
     <div className="members">
+      <div className="members-text">
+        <h1 className="members-page-title">Our team is comprised of genuinely gifted minds</h1>
+        <h5 className="members-page-text">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur voluptas doloribus incidunt ea expedita, ratione
+          id laudantium dicta, aperiam, consequuntur sequi sint a nobis harum non possimus. Quae ducimus iste itaque, iure, quo praesentium
+          consectetur impedit alias consequuntur, officiis recusandae ut ratione id fugit voluptatum voluptas sed animi fuga molestiae
+          officia unde ipsum sunt. Rerum fuga eaque praesentium ducimus optio dicta corporis libero nisi consectetur dolorem, qui aliquam,
+          deleniti ipsum amet, alias voluptatum fugiat soluta quaerat tenetur aliquid pariatur necessitatibus et blanditiis consequuntur?
+          Repudiandae qui ducimus illum ex aperiam facere, culpa quae maiores reiciendis est libero assumenda minima nihil.
+        </h5>
+      </div>
       <div className="member-cards">
-        {config.MembersList.map((member, index) => (
-          <MemberCard key={index} name={member.name} role={member.role} description={member.description} />
+        {MembersList.map((member, index) => (
+          <div key={index}>
+            {loadedImages[member.name] && (
+              <MemberCard name={member.name} role={member.role} description={member.description} image={loadedImages[member.name]} />
+            )}
+          </div>
         ))}
       </div>
     </div>
