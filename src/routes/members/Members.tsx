5b932d6c431c604e2848ddbb08023737aabd1599
Neil <elkadineil@gmail.com>
2023-08-10 09:36:04 -0400

    Added retired section & improved logic

--- src/routes/members/Members.tsx
+++ src/routes/members/Members.tsx
@@ -2,57 +2,78 @@ import React, { useState, useEffect } from "react";
 import MemberCard from "../../components/memberCard/MemberCard";
 import config from "../../config.json";
 import "./Members.scss";
-
-interface Member {
-  name: string;
-  role: string;
-  description: string;
-  image: string;
-}
-
-interface Config {
-  MembersList: Member[];
-}
+import { Member } from "../../utilities/types";
+import defaultUser from '../../img/defaultuser.jpg';
 
 export default function Members() {
-  const { MembersList } = config as Config;
+  const OfficerList = config.Officers as Member[];
+  const RetiredOfficerList = config.RetiredOfficers as Member[];
+  const MembersOfHonorList = config.MembersOfHonor as Member[];
+  const MembersList = config.Members as Member[];
+
   const [loadedImages, setLoadedImages] = useState<{ [key: string]: string }>({});
 
+  function MappedList(list: Member[]) {
+    return list.map((member, index) => (
+      <div key={index} className="grid-item">
+        {loadedImages[member.name] && (
+          <MemberCard member={member} imageUrl={loadedImages[member.name]} />
+        )}
+      </div>
+    ))
+  }
+
+  function LoadImages(people: Member[]) {
+    return people.map((member) =>
+      import(`../../img/${member.image}`).then((image) => {
+        setLoadedImages((prevImages) => ({
+          ...prevImages,
+          [member.name]: image.default,
+        }));
+      }).catch(() => {
+        setLoadedImages((preList) => ({
+          ...preList,
+          [member.name]: defaultUser,
+        }));
+      })
+    )
+  }
+
   useEffect(() => {
-    Promise.all(
-      MembersList.map((member) =>
-        import(`../../img/${member.image}`).then((image) => {
-          setLoadedImages((prevImages) => ({
-            ...prevImages,
-            [member.name]: image.default,
-          }));
-        })
-      )
+    Promise.all(LoadImages(OfficerList));
+    Promise.all(LoadImages(RetiredOfficerList));
+    Promise.all(LoadImages(MembersOfHonorList));
+    Promise.all(LoadImages(MembersList));
+  }, [OfficerList, RetiredOfficerList, MembersOfHonorList, MembersList]);
+
+  function MembersSection(props: { classes: string, title: string, list: Member[] }) {
+    return (
+      <div>
+        <h2><b>{props.title}</b></h2>
+        <div className={props.classes}>
+          {MappedList(props.list)}
+        </div>
+      </div>
     );
-  }, [MembersList]);
+  }
 
   return (
     <div className="members">
       <div className="members-text">
-        <h1 className="members-page-title">Our team is comprised of genuinely gifted minds</h1>
-        <h5 className="members-page-text">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus consequatur voluptas doloribus incidunt ea expedita, ratione
-          id laudantium dicta, aperiam, consequuntur sequi sint a nobis harum non possimus. Quae ducimus iste itaque, iure, quo praesentium
-          consectetur impedit alias consequuntur, officiis recusandae ut ratione id fugit voluptatum voluptas sed animi fuga molestiae
-          officia unde ipsum sunt. Rerum fuga eaque praesentium ducimus optio dicta corporis libero nisi consectetur dolorem, qui aliquam,
-          deleniti ipsum amet, alias voluptatum fugiat soluta quaerat tenetur aliquid pariatur necessitatibus et blanditiis consequuntur?
-          Repudiandae qui ducimus illum ex aperiam facere, culpa quae maiores reiciendis est libero assumenda minima nihil.
+        <h1 className="members-page-title flex-center"><b>Meet The Club Members</b></h1>
+        <h5 className="members-page-text flex-center">
+          Building a community is a collaborative effort partaken by some of the coolest people on campus.
+          Except for that Neil guy, he's just kinda okay.
         </h5>
       </div>
-      <div className="member-cards">
-        {MembersList.map((member, index) => (
-          <div key={index}>
-            {loadedImages[member.name] && (
-              <MemberCard name={member.name} role={member.role} description={member.description} image={loadedImages[member.name]} />
-            )}
-          </div>
-        ))}
-      </div>
+      {OfficerList.length > 0 && 
+      <MembersSection classes="member-cards current-officers" title="Officers" list={OfficerList}/>}
+      {RetiredOfficerList.length > 0 && 
+      <MembersSection classes="member-cards retired-officers" title="Retired Officers" list={RetiredOfficerList}/>}
+      {MembersOfHonorList.length > 0 && 
+      <MembersSection classes="member-cards honor" title="Members Of Honor" list={MembersOfHonorList}/>}
+      {MembersList.length > 0 && 
+      <MembersSection classes="member-cards members" title="Members" list={MembersList}/>}
     </div>
   );
 }
