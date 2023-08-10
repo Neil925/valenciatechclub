c306789b4c849adcd4abb17bc2ae57eaf08a3da3
Neil <elkadineil@gmail.com>
2023-08-10 04:34:50 -0400

    Home Page Complete

--- src/routes/home/HomePage.tsx
+++ src/routes/home/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import "./HomePage.scss";
 import config from "../../config.json";
+import { Link } from 'react-router-dom';
 
 export default function HomePage() {
   function randomHook() {
@@ -12,25 +13,27 @@ export default function HomePage() {
       <div className="header-hook flex-center">
         <div className="content-box flex-center-col">
           {randomHook()}
-          <button className='join-us'>
-            Join us!
-          </button>
+          <Link to="/join-us">
+            <button className='join-us'>
+              Join us!
+            </button>
+          </Link>
         </div>
       </div>
       <div className="about-us">
         <div className="text-box">
           <div className="paragraph">
-            <h1 className='section'><b>Valencia Technology Club</b></h1>
-            <p>Valencia Technology Club is a palce dedicated to building a friendly environment for 
+            <h1 className='section'><b>About Tech Club</b></h1>
+            <p>Valencia Technology Club is a palce dedicated to building a friendly environment for
               fellow tech majors to meet, network, and motivate one another in persuit of our career path.
-              <br/><br/>
+              <br /><br />
               We provide various engaging activities such as: programming workshops, desktop building, and
-              even resume support. Club members are always welcome to come hangout, work together, or tutor 
+              even resume support. Club members are always welcome to come hangout, work together, or tutor
               on none event days.</p>
           </div>
         </div>
         <div className="image-box">
-          <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft3.ftcdn.net%2Fjpg%2F00%2F72%2F98%2F56%2F360_F_72985661_LU1Xk0YQiPBwOuesuuJgwTn0NPlwP8ob.jpg&f=1&nofb=1&ipt=59b8172b4578e1770cf8af435f648f483eb6ce079a8a110a1012e5fb02fa7d87&ipo=images" />
+          <img src="./logo512.png" />
         </div>
       </div>
     </div>
