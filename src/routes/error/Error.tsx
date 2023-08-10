7c46b2a3abe850bec2e5cecffec013f2a1641ff0
Damien <DamienB2474@protonmail.com>
2023-08-10 05:31:38 -0400

    updated error page

--- src/routes/error/Error.tsx
+++ src/routes/error/Error.tsx
@@ -1,18 +1,35 @@
-import React from 'react'
-import './Error.scss';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Error.scss";
+import { Link } from "react-router-dom";
 
-function Error() {
+function Normal() {
   return (
     <div className="error flex-center-col">
-      <div>
-        <Link to='/'><h1>Go Back <span>Home</span>?</h1></Link>
-        {(Math.random() > .01) ? 
-        <p>No page to be found here...</p> : 
-        <p>Only <Link to='/the-eminence-in-shadow'>shadows</Link> to be found here...</p>}
-      </div>
+      <Link to="/">
+        <h1>
+          Go Back <span>Home</span>?
+        </h1>
+      </Link>
+      <p>No page to be found here...</p>
     </div>
-  )
+  );
+}
+
+function Shadow() {
+  return (
+    <div className="shadow-error flex-center-col">
+      <h1>
+        We lurk in the darkness and hunt{" "}
+        <strong>
+          <Link to="/the-eminence-in-shadow">Shadows</Link>
+        </strong>
+      </h1>
+    </div>
+  );
+}
+
+function Error() {
+  return Math.random() > 0.01 ? <Normal /> : <Shadow />;
 }
 
 export default Error;
