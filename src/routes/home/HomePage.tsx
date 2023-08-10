7dabd57efffd7733b0b5ba960e0f84dad305e71c
Neil <elkadineil@gmail.com>
2023-08-10 02:30:20 -0400

    About us gradient and image placement

--- src/routes/home/HomePage.tsx
+++ src/routes/home/HomePage.tsx
@@ -17,16 +17,19 @@ export default function HomePage() {
           </button>
         </div>
       </div>
-      <div className="about-us flex">
+      <div className="about-us">
         <div className="text-box">
           <div className="paragraph">
             <h1 className='section'><b>Valencia Technology Club</b></h1>
-            <p>text</p>
+            <p>Valencia Technology Club is a palce dedicated to building a friendly environment for 
+              fellow tech majors to meet, network, and motivate one another in persuit of our career path.
+              <br/><br/>
+              We provide various engaging activities such as: programming workshops, desktop building, and
+              even resume support. Club members are always welcome to come hangout, work together, or tutor 
+              on none event days.</p>
           </div>
         </div>
-        <div className="image-box ">
-          {/* OG image didn't load for me, so just found the same one but this did IDK ¯\_(ツ)_/¯  */}
-          {/* <img src="https://www.shutterstock.com/shutterstock/photos/229793107/display_1500/stock-photo-multi-ethnic-group-of-people-and-activity-concept-229793107.jpg" /> */}
+        <div className="image-box">
           <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ft3.ftcdn.net%2Fjpg%2F00%2F72%2F98%2F56%2F360_F_72985661_LU1Xk0YQiPBwOuesuuJgwTn0NPlwP8ob.jpg&f=1&nofb=1&ipt=59b8172b4578e1770cf8af435f648f483eb6ce079a8a110a1012e5fb02fa7d87&ipo=images" />
         </div>
       </div>
