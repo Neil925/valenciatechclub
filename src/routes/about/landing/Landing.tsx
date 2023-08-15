79e26b31d62767ff778bce404bc3b62452b71e45
Damien <DamienB2474@protonmail.com>
2023-08-15 09:07:27 -0400

    join us refactor

--- src/routes/about/landing/Landing.tsx
+++ src/routes/about/landing/Landing.tsx
@@ -3,8 +3,8 @@ import "./Landing.scss";
 
 export default function Shadow() {
   return (
-    <div className="shadow-main flex-top">
-      <div className="shadow-header">
+    <div className="shadow-main ">
+      <div className="shadow-header flex-top">
         <h1 className="diablos-intro">
           The Cult of Diablos... That's our <span className="enemy-span">enemy</span>.
         </h1>
--- src/routes/join/Join.tsx
+++ src/routes/join/Join.tsx
@@ -3,42 +3,74 @@ import "./Join.scss";
 
 export default function Join() {
   return (
-    <div className="join-us">
-      <div className="where-and-when flex">
-        <div className="where flex-center-col">
-          <h2>Where?</h2>
-          <h4>Valencia East Campus Building 8 Room 221</h4>
-        </div>
-        <div className="when flex-center-col">
-          <h2>When?</h2>
-          <h4>Every Wednesday starting at 1pm and going to 4pm</h4>
+    /*
+        TODO: Give cards glow
+        TODO: Change emojis with FA icons/pngs
+    */
+
+    <div className="join-us-div">
+      <div className="whitespace"></div>
+      <div className="join-us-ribbon flex-center-col">
+        <div className="join-us-ribbon-text flex-center-col">
+          <h1>📣 Join Us at Valencia Tech Club! 🚀</h1>
+          <h3>
+            Are you passionate about all things tech? Eager to explore the latest advancements, share your insights, and connect with
+            like-minded enthusiasts? Look no further than the Valencia Tech Club! 🌟
+          </h3>
         </div>
       </div>
-
-      <div className="ribbon"></div>
-
-      <div className="discord-engage-box flex">
-        <div className="tech-logo-div">
-          <img src="./logo512.png" alt="Tech Club logo" />
+      <div className="location flex">
+        <div className="where flex-center-col">
+          <h3>🗺️ Where: Valencia East Campus, Building 8, Room 221</h3>
         </div>
-        <div className="discord-and-engage flex">
-          <div className="discord">
-            <h2>
-              Come check out the{" "}
-              <a href="https://www.discord.gg/RRkWy4fVMv" target="_blank">
-                Discord
-              </a>{" "}
-              and join the discussion!
-            </h2>
+        <div className="logo flex-center-col">
+          <img src="./logo192.png" alt="vTechClub logo" />
+        </div>
+        <div className="when flex-center-col">
+          <h3>📅 When: Every Wednesday, from 1:00 PM to 4:00 PM</h3>
+        </div>
+      </div>
+      <div className="join-us-body flex">
+        <div className="expect flex-center-top">
+          <h4>What to expect:</h4>
+          <div className="list-box flex-center-col">
+            <ul className="no-bullets ">
+              <li>
+                <h5>🗣️ Engaging discussions on tech trends</h5>
+              </li>
+              <li>
+                <h5>🔧 Hands-on workshops and showcases</h5>
+              </li>
+              <li>
+                <h5>🤝 Networking with fellow tech lovers</h5>
+              </li>
+              <li>
+                <h5>💡 Brainstorming innovative projects</h5>
+              </li>
+              <li>
+                <h5>🎯 Insightful discussions about tech career paths</h5>
+              </li>
+            </ul>
           </div>
-          <div className="engage">
-            <h2>
-              Stay up to date with our newest{" "}
-              <a href="https://valenciacollege.campuslabs.com/engage/organization/valenciatechclub/events" target="_blank">
-                Events
-              </a>{" "}
-              through Engage!
-            </h2>
+        </div>
+        <div className="discord flex-center-col">
+          <div className="card flex">
+            <h2>Join Us on Discord</h2>
+            <p>Stay connected, share ideas, and network with fellow tech enthusiasts.</p>
+            <a href="https://www.discord.gg/RRkWy4fVMv" target="_blank" rel="noopener noreferrer">
+              Join Discord
+            </a>
+          </div>
+          <div className="card flex">
+            <h2>Explore Upcoming Events</h2>
+            <p>Discover exciting workshops, presentations, and tech discussions.</p>
+            <a
+              href="https://valenciacollege.campuslabs.com/engage/organization/valenciatechclub/events"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Explore Engage
+            </a>
           </div>
         </div>
       </div>
